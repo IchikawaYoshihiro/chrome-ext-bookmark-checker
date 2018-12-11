b137cd22c9093ccdea1ee955e36fccefa8f099cf
IchikawaYoshihiro <ichikawa.y.ac@gmail.com>
2018-12-11 17:49:35 +0900

    add check logic

--- js/popup.js
+++ js/popup.js
@@ -1,87 +1,66 @@
-// console.log('popup!');
-
-// const app = {
-//   $app: null,
-//   init() {
-//     this.$app = $('#app');
-//   },
-//   fetchBookmarks() {
-//     return chrome.bookmarks.getTree();
-//   },
-//   render(bookmarks) {
-//     this.$app.html(
-//       bookmarks.reduce((html, b) => {
-//         return `${ html }
-//         <tr>
-//           <td>
-//           </td>
-//           <td>
-//             <div>${ b.folder }${ b.title || 'no title' }</div>
-//             <div><a href="${ b.url }">${ b.url }</a></div>
-//           </td>
-//         </tr>`;
-//       }, ''))
-//   },
-//   flatten(bookmark, folder = '') {
-//     if (bookmark && bookmark.children) {
-//       return bookmark.children.reduce((arr, b) => {
-//         return arr.concat(app.flatten(b, `${folder}${bookmark.title}/`));
-//       }, []);
-//     }
-//     bookmark.folder = folder
-//     return [bookmark];
-//   }
-
-// };
-
-
-// (async function () {
-//   app.init();
-
-//   const bookmarks = await app.fetchBookmarks();
-
-//   console.log(bookmarks[0]);
-
-//   const f = app.flatten(bookmarks[0]);
-
-//   console.log(f);
-
-//   app.render(f)
-
-// })();
-
-
-var app = new Vue({
+const app = new Vue({
   el: '#app',
   data: {
     bookmarks: [],
   },
   methods: {
+    /**
+     * fetch bookmark tree data.
+     * @return {Promise}
+     */
     fetchBookmarks() {
       return chrome.bookmarks.getTree();
     },
+    /**
+     * flatten bookmark tree.
+     * @param {Object} bookmark bookmark tree
+     * @param {String} folder parent folder name
+     * @return {Array} bookmarks
+     */
     flatten(bookmark, folder = '') {
       if (bookmark && bookmark.children) {
         return bookmark.children.reduce((arr, b) => {
           return arr.concat(this.flatten(b, `${folder}${bookmark.title}/`));
         }, []);
       }
+
+      // parent folder name
       bookmark.folder = folder
+      // for HTTP status
+      bookmark.status = null;
+
       return [bookmark];
     },
+    /**
+     * build the bookmark title with parents folder title.
+     * @param {Object} b bookmark object
+     * @return {String}
+     */
     fullPath(b) {
       return `${b.folder}${ b.title || 'no title' }`;
-    }
+    },
+
+    async reload() {
+      this.bookmarks = [];
+      const bookmarks = await this.fetchBookmarks();
+
+
+      const f = this.flatten(bookmarks[0]);
+
+
+      this.bookmarks = f;
+    },
+    check() {
+      this.bookmarks.map(async b => {
+        const res = await fetch(b.url);
+
+        b.status = res.ok;
+
+        return b;
+      });
+    },
   },
-  async created() {
-    const bookmarks = await this.fetchBookmarks();
-
-    console.log(bookmarks);
-
-    const f = this.flatten(bookmarks[0]);
-
-    console.log(f);
-
-    this.bookmarks = f;
+  created() {
+    this.reload();
   }
 })
