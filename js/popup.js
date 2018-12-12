4eb91a367979f19ee196f20718bf71374c0fe585
IchikawaYoshihiro <ichikawa.y.ac@gmail.com>
2018-12-12 14:21:34 +0900

    add delete logic

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,29 @@
+Vue.component('bookmark-component', {
+  template: '#bookmark-component',
+  props: {
+    bookmark: Object,
+  },
+  computed: {
+    /**
+     * build the bookmark title with parents folder title.
+     * @return {String}
+     */
+    fullPath() {
+      return `${this.bookmark.folder}${this.bookmark.title || 'no title'}`;
+    },
+    statusText() {
+      return `${this.bookmark.status || '-'}`;
+    },
+    statusClass() {
+      return {
+        'status-ok': this.bookmark.ok === true,
+        'status-ng': this.bookmark.ok === false,
+        'status-none': this.bookmark.ok !== true && this.bookmark.ok !== false,
+      }
+    },
+  },
+})
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -28,18 +54,10 @@ const app = new Vue({
       bookmark.folder = folder
       // for HTTP status
       bookmark.status = null;
+      bookmark.ok = null;
 
       return [bookmark];
     },
-    /**
-     * build the bookmark title with parents folder title.
-     * @param {Object} b bookmark object
-     * @return {String}
-     */
-    fullPath(b) {
-      return `${b.folder}${ b.title || 'no title' }`;
-    },
-
     async reload() {
       this.bookmarks = [];
       const bookmarks = await this.fetchBookmarks();
@@ -47,18 +65,39 @@ const app = new Vue({
 
       const f = this.flatten(bookmarks[0]);
 
+      console.log(f);
 
       this.bookmarks = f;
     },
     check() {
       this.bookmarks.map(async b => {
-        const res = await fetch(b.url);
-
-        b.status = res.ok;
-
-        return b;
+        let res;
+        try {
+          console.log(b.url);
+          res = await fetch(b.url);
+        } catch (e) {
+          console.warn(e);
+        }
+        finally {
+          if (res) {
+            b.status = res.status || 0;
+            b.ok = res.ok || false;
+          }
+          return b;
+        }
       });
     },
+    /**
+     * delete a bookmark
+     * @param {String} index bookmarklist id(NOT bookmark.id)
+     */
+    async del(index) {
+      if (window.confirm('are you sure?')) {
+        const bookmark = this.bookmarks[index];
+        await chrome.bookmarks.remove(bookmark.id);
+        this.bookmarks = this.bookmarks.filter((b, i) => i !== index);
+      }
+    }
   },
   created() {
     this.reload();
